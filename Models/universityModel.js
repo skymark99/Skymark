1275ccd9110f93fbd3dca5c0f6d40b22761d9306
Arjun CV <arjuncv@Arjuns-MacBook-Air.local>
2024-10-03 01:12:10 +0530

    changed branch to branchName in universityModal because api featues will ignore branch keyword

--- Models/universityModel.js
+++ Models/universityModel.js
@@ -1,71 +1,74 @@
 const mongoose = require("mongoose");
 const { combineDateWithCurrentTime } = require("../Utilities/helper");
 
-const universitySchema = mongoose.Schema({
-  date: {
-    type: Date,
-    default: new Date(),
-  },
-  student: {
-    type: String,
-    required: [true, "University Data must have student name"],
-  },
-  branch: {
-    type: String,
-    enum: [
-      "Kozhikode",
-      "Kottayam",
-      "Kochi",
-      "Manjeri",
-      "Kannur",
-      "Directors",
-      "Corporate",
-    ],
-    required: [true, "Branch must have a name"],
-  },
+const universitySchema = mongoose.Schema(
+  {
+    date: {
+      type: Date,
+      default: new Date(),
+    },
+    student: {
+      type: String,
+      required: [true, "University Data must have student name"],
+    },
+    branchName: {
+      type: String,
+      enum: [
+        "Kozhikode",
+        "Kottayam",
+        "Kochi",
+        "Manjeri",
+        "Kannur",
+        "Directors",
+        "Corporate",
+      ],
+      required: [true, "Branch must have a name"],
+    },
 
-  formattedDate: {
-    type: String,
+    formattedDate: {
+      type: String,
+    },
+    counsillor: {
+      type: String,
+      required: [true, "University Data must have a counsellor"],
+    },
+    country: {
+      type: String,
+      required: [true, "University Data must have a Country"],
+    },
+    intakeMonth: {
+      type: String,
+      required: [true, "University Data must have an intake month"],
+    },
+    intake: {
+      type: String,
+      required: [true, "University must have an intake"],
+      enum: ["April-October", "November-March"],
+    },
+    university: {
+      type: String,
+      required: [true, "University must have a name"],
+    },
+    courseFee: {
+      type: Number,
+      required: [true, "University must have a course fee"],
+    },
+    commition: {
+      type: Number,
+      required: [true, "University must have a commition"],
+    },
+    status: {
+      type: String,
+      required: [true, "University must have a status"],
+      enum: ["Invoice Shared", "Mail Pending", "Pending", "Recieved"],
+    },
+    agent: {
+      type: String,
+      required: [true, "University Must have an agent name"],
+    },
   },
-  counsillor: {
-    type: String,
-    required: [true, "University Data must have a counsellor"],
-  },
-  country: {
-    type: String,
-    required: [true, "University Data must have a Country"],
-  },
-  intakeMonth: {
-    type: String,
-    required: [true, "University Data must have an intake month"],
-  },
-  intake: {
-    type: String,
-    required: [true, "University must have an intake"],
-    enum: ["April-October", "November-March"],
-  },
-  university: {
-    type: String,
-    required: [true, "University must have a name"],
-  },
-  courseFee: {
-    type: Number,
-    required: [true, "University must have a course fee"],
-  },
-  commition: {
-    type: Number,
-    required: [true, "University must have a commition"],
-  },
-  status: {
-    type: String,
-    required: [true, "University must have a status"],
-    enum: ["Invoice Shared", "Mail Pending", "Pending", "Recieved"],
-  },
-  agent: {
-    type: String,
-    required: [true, "University Must have an agent name"],
-  },
-});
+  { timestamps: true }
+);
 
 universitySchema.pre("save", async function (next) {
   const combinedDateTime = combineDateWithCurrentTime(this.date);
